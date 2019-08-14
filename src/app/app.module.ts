35507fcb0564502707c5de254df966ada6746a1a
Dino Horvat <dxh3401@rit.edu>
2019-08-14 08:59:30 +0200

    Bootstraping the custom elemenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 import { CustomHeaderComponent } from './components/custom-header/custom-header.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,19 @@ import { CustomHeaderComponent } from './components/custom-header/custom-header.
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CustomButtonComponent, CustomHeaderComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(CustomButtonComponent,
+      { injector: this.injector });
+    customElements.define('my-button-element', el);
+    const el2 = createCustomElement(CustomHeaderComponent,
+      { injector: this.injector });
+    customElements.define('custom-header', el2);
+  }
+}
